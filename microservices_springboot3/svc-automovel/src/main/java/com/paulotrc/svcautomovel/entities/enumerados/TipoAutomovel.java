c83fc3cf3b1193d7be70eb5a2c8f5df9928433ec
Paulo Carneiro <paulotrc@gmail.com>
2023-06-28 09:07:48 -0300

    [NEW] Implementation using Springboot 3 and Openapi 3 Entities

---
+++ microservices_springboot3/svc-automovel/src/main/java/com/paulotrc/svcautomovel/entities/enumerados/TipoAutomovel.java
@@ -0,0 +1,13 @@
+package com.paulotrc.svcautomovel.entities.enumerados;
+
+public enum TipoAutomovel {
+
+    CARRO,
+    MOTO,
+    SUV,
+    CAMINHONETE,
+    VAN,
+    CAMINHAO,
+    CARRETINHA,
+    ONIBUS;
+}
