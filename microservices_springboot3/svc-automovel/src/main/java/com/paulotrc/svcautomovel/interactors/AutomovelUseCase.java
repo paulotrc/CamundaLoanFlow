8d9a9ce88ea587a3375fd7d43800953d28b54e9b
Paulo Carneiro <paulotrc@gmail.com>
2023-06-28 09:08:22 -0300

    [NEW] Implementation using Springboot 3 and Openapi 3 Interactors

---
+++ microservices_springboot3/svc-automovel/src/main/java/com/paulotrc/svcautomovel/interactors/AutomovelUseCase.java
@@ -0,0 +1,39 @@
+package com.paulotrc.svcautomovel.interactors;
+
+import com.paulotrc.svcautomovel.entities.Automovel;
+import com.paulotrc.svcautomovel.repositories.AutomovelRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AutomovelUseCase {
+
+    private AutomovelRepository automovelRepository;
+
+    public AutomovelUseCase(AutomovelRepository automovelRepository) {
+        this.automovelRepository = automovelRepository;
+    }
+
+    public Automovel gravarAutomovel(Automovel automovel) {
+        return automovelRepository.save(automovel);
+    }
+
+    public List<Automovel> consultaTodos() {
+        return automovelRepository.findAll();
+    }
+
+    public List<Automovel> consultarPorCpf(String cpf) {
+        return automovelRepository.consultarPorCpf(cpf);
+    }
+
+    public List<Automovel> consultarPorPlaca(String cep) {
+        return automovelRepository.consultarPorPlaca(cep);
+    }
+
+    public Boolean cepExiste(String cep){
+
+        return false;
+    }
+
+}
