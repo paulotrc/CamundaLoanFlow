266193a42de8cc335315176fdf97e39ff6c03b1f
Paulo Carneiro <paulotrc@gmail.com>
2023-06-28 09:07:28 -0300

    [NEW] Implementation using Springboot 3 and Openapi 3 DataSources

---
+++ microservices_springboot3/svc-automovel/src/main/java/com/paulotrc/svcautomovel/datasources/AutomovelDataSource.java
@@ -0,0 +1,37 @@
+package com.paulotrc.svcautomovel.datasources;
+
+import com.paulotrc.svcautomovel.entities.Automovel;
+import com.paulotrc.svcautomovel.repositories.AutomovelRepository;
+import com.paulotrc.svcautomovel.repositories.MongoAutomovelRepository;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class AutomovelDataSource implements AutomovelRepository {
+
+    private final Logger log = LoggerFactory.getLogger(AutomovelDataSource.class);
+    private MongoAutomovelRepository mongoAutomovelRepository;
+
+    public Automovel save(Automovel automovel){
+        return this.mongoAutomovelRepository.save(automovel);
+    }
+
+    public List<Automovel> findAll(){
+        return this.mongoAutomovelRepository.findAll();
+    }
+
+    @Override
+    public List<Automovel> consultarPorCpf(String cpf) {
+        return mongoAutomovelRepository.consultarPorCpf(cpf);
+    }
+
+    @Override
+    public List<Automovel> consultarPorPlaca(String placa) {
+        return mongoAutomovelRepository.consultarPorPlaca(placa);
+    }
+}
